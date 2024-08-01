bc11040dc93e0be24935c2d22f3c470af5aa3b3a
Davi Nunes <sallyn2544@gmail.com>
2024-08-01 15:52:54 -0300

    System of points and system of radom

--- quiz.c
+++ quiz.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 #define CHOICES 3
 
 void welcome();
-int win(int, int);
+double lostPoints(int, int);
+
+int random();
 
 int main(){
 
     welcome();
 
-    int numberSecret = 42, choice = 0, hit = 0, try = 0;
-    
+    int numberSecret = 42, choice = 0,  try = 0;
+
+    double points = 1000.00;
 
 
-    while(!hit){
+    // use function to return value random
+    numberSecret = random();
+
+
+
+    while(1){
 
         printf("Your %d chance: ", try+1);
         scanf("%d", &choice);
@@ -30,7 +40,7 @@ int main(){
 
         if(win){
             printf("\nCongratulations!\n\n");
-            hit = 1;
+            break;
         }
         else if(greather){
             printf("\nYour choice is greather than the number secret\n\n");
@@ -39,19 +49,20 @@ int main(){
             printf("\nYour choice is less than the number number secret\n\n");
         }
 
+        points -= lostPoints(numberSecret,choice);
         try ++;
 
     }
 
-    printf("Quantity of try: %d", try);
+    printf("Quantity of try: %d\n", try);
 
-    
+    printf("Your pontuation: %.2lf\n", points);
+
+
 
 
     return 0;
 }
-
-
 
 void welcome(){
 
@@ -60,3 +71,21 @@ void welcome(){
        printf("************************************\n\n");
 }
 
+double lostPoints(int numberSecret, int choice){
+
+    double pointsLost =  (double) (numberSecret - choice) / 2.00;
+
+    // function to value absolute of numbers double
+    return fabs(pointsLost);
+
+
+}
+
+int random(){
+
+    // We set the seed to the current time value
+    srand(time(0));
+
+    // return number random beetwen 0 and 99
+    return rand() % 100;
+}

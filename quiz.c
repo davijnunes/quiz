1f8581300785d8259fb1a9051ceba1d86e857183
Davi  Nunes <144159901+davijnunes@users.noreply.github.com>
2024-08-01 10:28:14 -0300

    Adicionei sistemas de pontos e aleatório
    Adicionei sistemas de pontos é sistemas de números aletórios.

--- quiz.c
+++ quiz.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 #define CHOICES 3
 
 void welcome();
@@ -10,10 +12,18 @@ int main(){
     welcome();
 
     int numberSecret = 42, choice = 0, hit = 0, try = 0;
-    
+    double point = 1000.00, pointLeft = 0.0;
+
+    // generator of numbers random
+    srand(time(0));
+
+    // Number random between 0 e 99
+    numberSecret = rand() % 100;
+
+    printf("Number secret: %d\n", numberSecret);
 
 
-    while(!hit){
+    while(1){
 
         printf("Your %d chance: ", try+1);
         scanf("%d", &choice);
@@ -30,7 +40,7 @@ int main(){
 
         if(win){
             printf("\nCongratulations!\n\n");
-            hit = 1;
+            break;
         }
         else if(greather){
             printf("\nYour choice is greather than the number secret\n\n");
@@ -39,13 +49,15 @@ int main(){
             printf("\nYour choice is less than the number number secret\n\n");
         }
 
+        pointLeft = (double)(numberSecret - choice) / 2.0;
+        point -= abs(pointLeft);
+
         try ++;
 
     }
 
-    printf("Quantity of try: %d", try);
-
-    
+    printf("Total of points: %.2lf\n", point);
+    printf("Quantity of try: %d\n", try);
 
 
     return 0;
@@ -59,4 +71,3 @@ void welcome(){
        printf("* Welcome to the game of quiz *\n");
        printf("************************************\n\n");
 }
-

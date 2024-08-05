5857ce36f81d72dd27e96af85252b28f0ee368ca
Davi Nunes <sallyn2544@gmail.com>
2024-08-05 18:05:26 -0300

    Melhoramos o nosso quiz, colocando tentativas correspondente para niveis, colocando msg para defeat e win, colocando um welcome melhor

--- quiz.c
+++ quiz.c
@@ -3,77 +3,110 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+
 #define CHOICES 3
+#define EASY 1
+#define AVERAGE 2
+#define DIFFICULTY 3
 
 // Prototypes
 void welcome();
-
 double lostPoints(int, int);
-
 int random();
+int attempsInGame();
 
 // Enter point
 int main(){
 
     welcome();
 
-    int numberSecret = 42, choice = 0,  try = 0;
+    int numberSecret = 42, choice = 0,  attempts = 0, win = 0, greather, runningGame = 0;
 
     double points = 1000.00;
-
 
     // use function to return value random
     numberSecret = random();
 
 
-    while(1){
 
-        printf("Your %d chance: ", try+1);
-        scanf("%d", &choice);
+    //printf("Debugging: %d\n",attempts);
+
+    printf("\n");
+
+    while(!runningGame){
+
+               // Use function to return quantity of attempts
+               attempts = attempsInGame();
+
+               printf("\n");
+
+        for(int i = 0; i < attempts; i++){
+
+            printf("-> Your %d chance: ", i+1);
+            scanf("%d", &choice);
+
+            printf("\n");
+
+            win = numberSecret == choice;
+            greather = choice > numberSecret;
+
+            if(choice < 0) {
+                printf("\n\nEnter a numbers positive!\n\n");
+                i--;
+                continue;
+            }
+
+            if(win){
+                break;
+            }
+            else if(greather){
+                printf("\nYour choice is greather than the number secret\n\n");
+            }
+            else{
+                printf("\nYour choice is less than the number number secret\n\n");
+            }
+
+            // Function return a quantity of  points lost
+            points -= lostPoints(numberSecret,choice);
 
 
-        int win = numberSecret == choice;
-        int greather = choice > numberSecret;
 
-        if(choice < 0) {
-            printf("\nEnter a numbers positive!\n\n");
-            continue;
         }
 
-
-        if(win){
-            printf("\nCongratulations!\n\n");
-            break;
-        }
-        else if(greather){
-            printf("\nYour choice is greather than the number secret\n\n");
-        }
-        else{
-            printf("\nYour choice is less than the number number secret\n\n");
-        }
-
-        points -= lostPoints(numberSecret,choice);
-        try ++;
+            printf("Want to play again, enter (0 to yes or 1 to no):  ");
+            scanf("%d", &runningGame);
 
     }
 
-    printf("Quantity of try: %d\n", try);
+    // Verify if user win or no
+        if(win){
+            printf("\nCogratulations!\n");
+        }
+        else{
+            printf("\nTry new!\n");
+        }
 
+    // Show punctuation
     printf("Your pontuation: %.2lf\n", points);
-
-
-
 
     return 0;
 }
 
+// Function to show welcome for user
 void welcome(){
 
-       printf("\n************************************\n");
-       printf("* Welcome to the game of quiz *\n");
-       printf("************************************\n\n");
+        printf("\n\n");
+        printf("          P  /_\\  P                              \n");
+        printf("         /_\\_|_|_/_\\                            \n");
+        printf("     n_n | ||. .|| | n_n         Bem vindo ao     \n");
+        printf("     |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação! \n");
+        printf("    |\" \"  |  |_|  |\"  \" |                     \n");
+        printf("    |_____| ' _ ' |_____|                         \n");
+        printf("          \\__|_|__/                              \n");
+        printf("\n\n");
 }
 
+// Calculation of points lost
 double lostPoints(int numberSecret, int choice){
 
     double pointsLost =  (double) (numberSecret - choice) / 2.00;
@@ -84,6 +117,7 @@ double lostPoints(int numberSecret, int choice){
 
 }
 
+// Generate number random
 int random(){
 
     // We set the seed to the current time value
@@ -93,3 +127,27 @@ int random(){
     return rand() % 100;
 }
 
+// return attemps
+int attempsInGame(){
+
+    int lvl = 0;
+
+    printf("What's is the difficulty level?\n");
+    printf("(1) Easy\n(2) Average\n(3) difficult\n\n");
+    printf("Choice: ");
+    scanf("%d", &lvl);
+
+
+    switch (lvl){
+
+        case EASY:
+            return 20;
+        case AVERAGE:
+            return 15;
+        case DIFFICULTY:
+            return 6;
+        default:
+            return 6;
+    }
+
+}
